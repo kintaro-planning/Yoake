c6e080089a4db6f2b758a906b79f6bc88e90640c
kintaro <info@kintaro-planning.co.jp>
2025-05-31 10:57:20 +0900

    single.php のデザイン変更

--- single.php
+++ single.php
@@ -1,10 +1,8 @@
 <?php
 /**
  * single.php
- *
  * ブログ記事の詳細ページテンプレート
  */
-
 get_header();
 ?>
 
@@ -12,7 +10,6 @@ get_header();
 
   <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
     <article id="post-<?php the_ID(); ?>" <?php post_class('single-post'); ?>>
-      <!-- 共通のセクション見出しスタイルを流用 -->
       <header class="section-header">
         <h2><?php the_title(); ?></h2>
         <div class="post-meta">
@@ -20,19 +17,17 @@ get_header();
             <?php echo esc_html( get_the_date() ); ?>
           </time>
           <?php if ( get_the_tag_list() ) : ?>
-            <div class="tags"><?php echo get_the_tag_list('',', '); ?></div>
+            <span class="tags"><?php echo get_the_tag_list('',', '); ?></span>
           <?php endif; ?>
         </div>
       </header>
 
-      <!-- アイキャッチ -->
       <?php if ( has_post_thumbnail() ) : ?>
         <div class="post-featured-image">
           <?php the_post_thumbnail('full'); ?>
         </div>
       <?php endif; ?>
 
-      <!-- 本文 -->
       <div class="post-content">
         <?php
           the_content();
